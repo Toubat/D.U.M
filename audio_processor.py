cb402512f669c73a2946aa916369e5123ab09434
Toubat <57741529+Toubat@users.noreply.github.com>
2021-08-07 21:10:13 +0800

    fix audio conversion bug, update conversion rule

--- audio_processor.py
+++ audio_processor.py
@@ -1,50 +1,24 @@
+import os 
+import numpy as np
 import librosa
 import librosa.display
-import matplotlib.pyplot as plt
-
-import os 
-
-import torch
-import torchaudio
-import torchaudio.transforms as T
-
-import numpy as np
 from scipy.io.wavfile import write
 
 
-class AudioToMelSpectrogram():
-    def __init__(self, sample_rate, n_fft, hop_length, n_mels):
+class AudioToMFCCs():
+    def __init__(self, sample_rate, n_mfcc):
         self.sample_rate = sample_rate
-        self.n_fft = n_fft
-        self.hop_length = hop_length
-        self.n_mels = n_mels
+        self.n_mfcc = n_mfcc
 
-    def audio_to_mel(self, audio_file='./refined_wav/mBR0.wav'):
-        waveform, sample_rate = torchaudio.load(audio_file)
+    def audio_to_mfcc(self, audio_dir):
+        audio, sample_rate = librosa.load(audio_dir, sr=self.sample_rate)
+        mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=self.n_mfcc)
 
-        resampler = T.Resample(sample_rate, self.sample_rate)
-        mel_spectrogram = T.MelSpectrogram(
-            sample_rate=self.sample_rate,
-            n_fft=self.n_fft,
-            hop_length=self.hop_length,
-            n_mels=self.n_mels,
-        )
+        return mfccs
 
-        waveform = resampler(waveform)
-        # (2, num_samples) -> (1, num_samples)
-        waveform = torch.mean(waveform, dim=0, keepdim=True)
-        mel_sgram = mel_spectrogram(waveform)
-        mel_sgram = librosa.amplitude_to_db(mel_sgram[0].numpy())
-
-        # librosa.display.specshow(mel_sgram, sr=22050, x_axis='time', y_axis='mel')  
-        # plt.colorbar(format='%+2.0f dB')
-
-        return mel_sgram
-
-    def mel_to_audio(self, mel_sgram):
-        # still under development (reducing noise needed!)
-        specgram = librosa.db_to_amplitude(mel_sgram)
-        audio = librosa.feature.inverse.mel_to_audio(specgram, n_fft=self.n_fft, hop_length=self.hop_length).reshape(1, -1)
+    def mfcc_to_audio(self, mfccs, audio_dir):
+        audio = librosa.feature.inverse.mfcc_to_audio(mfccs, sr=self.sample_rate)
+        write(audio_dir, self.sample_rate, audio.squeeze())
 
         return audio
         
@@ -52,17 +26,14 @@ class AudioToMelSpectrogram():
 
 def main():
     # The following line of code is only needed for windows 
-    #torchaudio.set_audio_backend("soundfile")
-    converter = AudioToMelSpectrogram(sample_rate=22050, n_fft=1024, hop_length=512, n_mels=128)    
-
-
+    converter = AudioToMFCCs(sample_rate=48000, n_mfcc=128)    
     directory = './refined_wav'
-    mel_directory = './npy_audio'
+    mfcc_directory = './audio_mfcc'
+
     for filename in os.listdir(directory):
-        mel_sgram = converter.audio_to_mel('/'.join([directory,filename]))
-        np.save('/'.join([mel_directory,'.'.join([os.path.splitext(filename)[0],'npy'])]), mel_sgram, allow_pickle=True, fix_imports=True)
-    
-    audio = converter.mel_to_audio(mel_sgram)
-    #write .wav to a specific file (not needed for now)
-    #write('test.wav', 22050, audio.squeeze())
+        mfccs = converter.audio_to_mfcc('/'.join([directory, filename]))
+        np.save('/'.join([mfcc_directory,'.'.join([os.path.splitext(filename)[0],'npy'])]), mfccs, allow_pickle=True, fix_imports=True)
+        # audio = converter.mfcc_to_audio(mfccs, 'test.wav')
+        assert mfccs.shape == (128, 938)
+
     return 0
