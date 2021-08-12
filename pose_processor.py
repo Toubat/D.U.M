599121d044c279c2c9bf8d5f569ee9801d66f2cf
Toubat <57741529+Toubat@users.noreply.github.com>
2021-08-12 17:51:31 +0800

    small changes

--- pose_processor.py
+++ pose_processor.py
@@ -11,8 +11,8 @@ class PoseDetector():
 
     def get_video_keypoints(self, path='./sample.mp4'):
         cap = cv2.VideoCapture(path)
-        keypoints = []
-        originals = []
+        keypoints = [[0] * 69]
+        originals = [[0] * 69]
         while cap.isOpened():
             _, img = cap.read()
             if (img is None):
@@ -21,13 +21,13 @@ class PoseDetector():
             img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             result = self.pose.process(img_RGB)
             if result.pose_landmarks:
-                # self.mp_draw.draw_landmarks(img, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+                self.mp_draw.draw_landmarks(img, result.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
                 for id, lm in enumerate(result.pose_landmarks.landmark):
                     if id in self.discard_points:
                         continue
                     height, width, channel = img.shape
                     cx, cy = int(lm.x * width), int(lm.y * height)
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+                    # cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
                     # normalize pkeypoints (value range between -1 and 1)
                     keypoint.append(lm.x * 2 - 1)
                     keypoint.append(lm.y * 2 - 1)
@@ -80,9 +80,9 @@ def video_show(originals, path='./sample.mp4'):
 
 
 def main():
-    path = 'path/to/video.mp4'
+    path = './sample.mp4'
     detector = PoseDetector()
-    keypoints, _ = detector.get_video_keypoints('https://s3.abci.ai/aistdancedb.ongaaccel.jp/v1.0.0/video/10M/gBR_sBM_c07_d04_mBR1_ch08.mp4')
+    keypoints, _ = detector.get_video_keypoints(path)
     print(keypoints.shape)
 
     return 0
